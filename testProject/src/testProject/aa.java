cc0e0580c6c8cb71c1b59b051a91daaf156aa31d
Deabaind <167089259+Deabaind@users.noreply.github.com>
2025-11-23 22:54:01 +0900

    Update aa.java

--- testProject/src/testProject/aa.java
+++ testProject/src/testProject/aa.java
@@ -4,28 +4,6 @@ import java.util.Arrays;
 
 public class aa {
 	public static void main(String[] args) {
-		String my_str = "12345678";
-		int n = 4;
 		
-		int arrayLength = my_str.length() / n;
-				
-		if (my_str.length() % n != 0) {
-			arrayLength++;
-		}
-		
-		String[] answer = new String[arrayLength];
-		
-		int dot = 0;
-		int k = 0;
-		
-		for (int i = 0; i < arrayLength; i++) {
-			k += n;
-			if (k >= my_str.length()) {
-				k = my_str.length();
-			}
-			answer[i] = my_str.substring(dot, k);
-			System.out.println("1. " + my_str.substring(dot, k));
-			dot += n;
-		}
 	}
 }
